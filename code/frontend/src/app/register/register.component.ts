09c3725cf55f8daabe5e89407f4e80d6ae0e5134
llaros <jdrulis@o2.pl>
2017-11-06 09:45:38 +0100

    validation

--- code/frontend/src/app/register/register.component.ts
+++ code/frontend/src/app/register/register.component.ts
@@ -4,12 +4,14 @@ import { Router } from '@angular/router';
 import { AlertService, UserService, DictService } from '../_services/index';
 import { User, CodeValue } from '../_models/index';
 import {SelectItem} from 'primeng/primeng'
+import {FormsModule} from '@angular/forms';
 
 @Component({
     templateUrl: 'register.component.html'
 })
 
 export class RegisterComponent {
+
     user: User = new User;
     loading = false;
 
@@ -24,7 +26,7 @@ export class RegisterComponent {
         private dictService: DictService) {
 
         this.user.roleCode = "EN";
-        this.user.roleCode = "WAW";
+        this.user.officeCode = "WAW";
 
         this.dictService.getRolesObs().subscribe((roles:CodeValue[]) => this.mapToSelectItem(roles, this.roles))
         this.dictService.getOfficesObs().subscribe((offices:CodeValue[]) => this.mapToSelectItem(offices, this.offices))
