3647177b33909be4c5244eaa4fa48cc43bae1558
koboldo@gmail.com <koboldo@gmail.com>
2017-12-13 11:39:48 +0100

    fix for data import

--- code/sql/runRecreate.sh
+++ code/sql/runRecreate.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-# set -x 
+set -x 
 # SET DIR FOR IMPORT DATA
-DATA_DIR=../../data
+# DATA_DIR=../../data
+# DB_DIR=../env_specific
+DATA_DIR=/home/botconsole/data
+DB_DIR=/home/botconsole/env_specific
 
-echo "Creating db structure ..."
-sqlite3 -init setup_db.sql ../env_specific/work-monitor.db ".exit"
-echo "Setting initial configuration ..."
-sqlite3 -init initdata.sql ../env_specific/work-monitor.db ".exit"
+sqlite3 -init setup_db.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init initdata.sql $DB_DIR/work-monitor.db ".exit"
 
 #TEST DATA USED ONLY TO CHECK API WITH SOAPUI
 # echo "Loading test data ..."
-# sqlite3 -init testdata.sql ../env_specific/work-monitor.db ".exit"
+# sqlite3 -init testdata.sql $DB_DIR/work-monitor.db ".exit"
 
 # REMOVE COMMENT BELOW TO IMPORT REAL DATA
-# sqlite3 -init beforeImport.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/WORK_TYPE_CODE_REFERENCE.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/WORK_TYPE.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/zlecajacy.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/ludzie.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/WO_WAR_LUKASZ.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/2017_RAPORT_1.sql ../env_specific/work-monitor.db ".exit"
-# sqlite3 -init $DATA_DIR/2017_RAPORT_2.sql ../env_specific/work-monitor.db ".exit"
+sqlite3 -init beforeImport.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/WORK_TYPE_CODE_REFERENCE.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/WORK_TYPE.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/zlecajacy.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/ludzie.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/WO_WAR_LUKASZ.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/2017_RAPORT_1.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init $DATA_DIR/2017_RAPORT_2.sql $DB_DIR/work-monitor.db ".exit"
+sqlite3 -init afterImport.sql $DB_DIR/work-monitor.db ".exit"
