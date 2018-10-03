70ef6da9a2d769c9ef4af7d1ea995a7dcd53da64
llaros <jdrulis@o2.pl>
2018-10-03 20:07:00 +0200

    cache expiry added

--- code/frontend/src/app/_services/httpCacheInterceptor.service.ts
+++ code/frontend/src/app/_services/httpCacheInterceptor.service.ts
@@ -10,39 +10,54 @@ export class HttpCacheInterceptor implements HttpInterceptor {
 
     private cacheUrlInfinixArr: string[] = ['/api/v1/codes', '/api/v1/persons', '/api/v1/workTypes'];
 
-    public remove(cacheUrlInfinix): void {
+    private put(cacheUrlInfinix: string, subject: AsyncSubject<HttpEvent<any>>) {
+        console.log("Adding to cache "+cacheUrlInfinix);
+        this.cache[cacheUrlInfinix] = subject;
+        setTimeout(() => {
+            this.remove(cacheUrlInfinix);
+        }, 600000); //10min
+    }
+
+    private remove(cacheUrlInfinix): void {
         delete this.cache[cacheUrlInfinix];
         console.log('Removed from cache '+cacheUrlInfinix);
     }
 
+    private getCachedInfinix(url: string):string {
+        for(let cacheUrlInfinix of this.cacheUrlInfinixArr) {
+            if (url.indexOf(cacheUrlInfinix) >= 0) {
+                return cacheUrlInfinix;
+            }
+        }
+        return null;
+    }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let url: string = request.urlWithParams;
+        let cacheKey : string = this.getCachedInfinix(url);
 
         if (request.method !== "GET") {
-            for(let cacheUrlInfinix of this.cacheUrlInfinixArr) {
-                if (url.indexOf(cacheUrlInfinix) >= 0) {
-                    console.log('Removing '+cacheUrlInfinix+' from cache due to '+request.method+' ['+url+']...');
-                    this.remove(cacheUrlInfinix);
-                }
+            if (cacheKey) {
+                console.log('Removing '+cacheKey+' from cache due to '+request.method+' on ['+url+']...');
+                this.remove(cacheKey);
             }
             return next.handle(request);
         }
 
-        for(let cacheUrlInfinix of this.cacheUrlInfinixArr) {
-            if (url.indexOf(cacheUrlInfinix) >= 0) {
+        const subject = new AsyncSubject<HttpEvent<any>>();
 
-                console.log('Checking cache for '+url);
+        if (cacheKey) {
+            console.log('Checking cache for '+cacheKey);
 
-                const cachedResponse = this.cache[url] || null;
-                if (cachedResponse) {
-                    console.log('Cache hit for '+url+'!');
-                    return cachedResponse.delay(0);
-                }
-
+            const cachedResponse = this.cache[cacheKey] || null;
+            if (cachedResponse) {
+                console.log('Cache hit for '+cacheKey+'!');
+                return cachedResponse.delay(0);
+            } else {
+                this.put(cacheKey, subject);
             }
         }
 
-        const subject = this.cache[url] = new AsyncSubject<HttpEvent<any>>();
         next.handle(request).do(event => {
             if (event instanceof HttpResponse) {
                 subject.next(event);
@@ -50,7 +65,6 @@ export class HttpCacheInterceptor implements HttpInterceptor {
             }
         }).subscribe(); // must subscribe to actually kick off request!
         return subject;
-
 
     }
 }
