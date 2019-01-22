de07226ee75961c0fa362193faf1e1965d61df9c
qukulele <biuro@rafalczarnecki.pl>
2019-01-22 20:16:26 +0100

    Dodanie zakładki Turbo table

--- code/frontend/src/app/wo-turbo-table/wo-turbo-table.component.ts
+++ code/frontend/src/app/wo-turbo-table/wo-turbo-table.component.ts
@@ -109,30 +109,30 @@ export class WoTurboTableComponent implements OnInit {
       
        this.search();
        this.cols = [
-           { field: 'officeCode', header: 'Biuro' , sortable: true, filter:true,class:"text-center"},
+           { field: 'officeCode', header: 'Biuro' , sortable: true, filter:true,class:"text-center-55"},
            { field: 'id', header: 'id', hidden: true, sortable: true, filter:true},
-           { field: 'workNo', header: 'Zlecenie', sortable: true, filter:true},
+           { field: 'workNo', header: 'Zlecenie', sortable: true, filter:true, class:"width-100"},
            { field: 'status', header: 'Status' , filter:true,statusCode:true, class:"width-135"},
-           { field: 'type', header: 'Typ', sortable:true, filter:true, type:true},
+           { field: 'type', header: 'Typ', sortable:true, filter:true, type:true, class:"width-135"},
            { field: 'complexityCode', header: 'Zł.', sortable:true, filter:false,complexity:true, icon:true,class:"text-center" },
            { field: 'complexity', header: 'Wycena' , sortable:true, filter:false,class:"text-center-55"},
-           { field: 'mdCapex', header: 'CAPEX', sortable:true, filter:true },
+           { field: 'mdCapex', header: 'CAPEX', sortable:true, filter:true,class:"width-100" },
            { field: 'price', header: 'Cena', sortable:true, filter:true,class:"width-70 text-right" },
            { field: 'sComments', header: 'Komentarz', hidden:true, sortable:true , filter:true},
            { field: 'description', header: 'Opis', hidden:true, filter:true },
-           { field: 'assignee', header: 'Wykonawca', sortable:true, filter:true },
+           { field: 'assignee', header: 'Wykonawca', sortable:true, filter:true, class:"width-100" },
            { field: 'isFromPool', header: 'Pula' , sortable:true, filter:true, isFromPool:true, icon:true, class:"text-center"},
-           { field: 'protocolNo', header: 'Protokół', sortable:true, filter:true },
-           { field: 'lastModDate', header: 'Mod.' , sortable:true, filter:true, class:"width-82"},
-           { field: 'creationDate', header: 'Utw.', sortable:true , filter:true, class:"width-82"},
-           { field: 'itemNo', header: 'Numer obiektu' , sortable:true, filter:true, class:"width-82"},
+           { field: 'protocolNo', header: 'Protokół', sortable:true, filter:true, class:"width-100" },
+           { field: 'lastModDate', header: 'Mod.' , sortable:true, filter:true, class:"width-100"},
+           { field: 'creationDate', header: 'Utw.', sortable:true , filter:true, class:"width-100"},
+           { field: 'itemNo', header: 'Numer obiektu' , sortable:true, filter:true, class:"width-125"},
            { field: 'itemBuildingType', header: 'Typ obiektu', hidden:true, sortable:true, filter:true },
            { field: 'itemConstructionCategory', header: 'Konstrukcja', hidden:true, sortable:true, filter:true },
            { field: 'itemAddress', header: 'Adres', hidden:true, sortable:true, filter:true },
            { field: 'itemDescription', header: 'Opis obiektu', hidden:true, sortable:true , filter:true},
-           { field: 'ventureCompany', header: 'Inwestor', sortable:true , filter:true},
-           { field: 'ventureDisplay', header: 'Zleceniodawca', sortable:true , filter:true },
-           { button: true, icone:true, class:"width-25"}
+           { field: 'ventureCompany', header: 'Inwestor', sortable:true , filter:true, class:"width-135"},
+           { field: 'ventureDisplay', header: 'Zleceniodawca', sortable:true , filter:true, class:"width-135" },
+           { button: true, icone:true, class:"width-35"}
           
            
        ];     
