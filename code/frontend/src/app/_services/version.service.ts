54d91cdc51a792e756336e0cdb53fa2578f4de84
llaros <jdrulis@o2.pl>
2020-05-25 15:32:01 +0200

    version change

--- code/frontend/src/app/_services/version.service.ts
+++ code/frontend/src/app/_services/version.service.ts
@@ -19,6 +19,7 @@ export class VersionService {
 
     constructor() {
         this._history = [
+            new CodeValue('1.5.64', 'Edycja excelId dla kontrahentów'),
             new CodeValue('1.5.63', 'Eksport wydajność zespołu'),
             new CodeValue('1.5.63', 'Dodanie typu zlecenia w widoku inżyniera - poprawka'),
             new CodeValue('1.5.62', 'Dodanie typu zlecenia w widoku inżyniera'),
